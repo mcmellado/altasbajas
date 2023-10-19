a1e5b59aac26ef2e078038079bb361cb313e81c2
Celeste <113931748+mcmellado@users.noreply.github.com>
2023-10-19 14:09:14 +0200

    mierdon

--- login.php
+++ login.php
@@ -1,83 +1,103 @@
-<?php session_start();
+<?php
+session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-?>
 
-<!DOCTYPE html>
-<html lang="es">
-
-<head class="header">
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="login.css">
-    <title>Login</title>
-    <div class="backGradient" id='header'>
-    <div height='auto'>
-        <img class='logo_cab' src='imagenes/logo_blanco.png'>
-        <h3>GESTIÓN DE BAJAS Y ALTAS</h3>
-    </div>
-    </div>
-</head>
-
-
-<body>
-<form action="" method="POST">
-
-<div class="acceso">
-    <p> ACCESO </p>
-</div>
-
-<div>
-    <input class="login" type="text" name="login" id="login" placeholder="Usuario">
-    <input class="password" type="password" name="password_login" id="password_login" placeholder="password">
-
-</div>
-<button type="submit" class="boton"> Entrar </button>
-</form>
-</body>
-</html>
-
-<?php
-
-// require 'conexion.php';
+require 'conexion.php'; 
 require 'auxiliar.php';
 
-$login          = obtener_post('login');
+$login = pg_escape_string($con, obtener_post('login'));
 $password_login = obtener_post('password_login');
 
-function comprobar($login, $password_login){
-    require 'conexion.php';
-    $comprobar_login    = pg_query($con, "SELECT * FROM usuarios WHERE usuario = '$login'");
+function comprobar($login, $password_login, $con) {
+    $comprobar_login = pg_query($con, "SELECT * FROM usuarios WHERE usuario = '$login'");
+    
+    if (!$comprobar_login) {
+        error_log("Error en la consulta: " . pg_last_error($con));
+        return "Error en la base de datos. Por favor, inténtalo de nuevo más tarde.";
+    }
 
-    if(pg_num_rows($comprobar_login) != 0){
+    if(pg_num_rows($comprobar_login) != 0) {
+        $fila = pg_fetch_assoc($comprobar_login);
+        $intentos_fallidos = $fila['intentos_fallidos'];
+        $bloqueo = $fila['bloqueado'];
 
         $datos_usuario = array(
             "usuario"            => $fila['usuario'],
             "password"           => $fila['password'],
             "intentos_fallidos"  => $fila['intentos_fallidos'],
-            "bloqueo"            => $fila['bloqueo'],
+            "bloqueo"            => $fila['bloqueado'],
             "fecha_nac"          => $fila['fecha_nacimiento'],
             "alta"               => $fila['alta'],
             "baja"               => $fila['baja'],
         );
 
-        if($password == $password_login) {
-            $_SESSION['datos_usuario'] = $datos_usuario;
-            header('Location: index.php');
-
-        } else {
-            echo "El usuario o contraseña es incorrecto";
+        if ($bloqueo) {
+            $tiempo = strtotime($fila['tiempo']);
+            $tiempo_actual = strtotime('now');
+            $diferencia_tiempo = $tiempo_actual - $tiempo;
+            if($diferencia_tiempo >= 900) {
+                pg_query($con, "UPDATE usuarios SET bloqueado = FALSE, intentos_fallidos = 0, tiempo = NULL WHERE usuario = '$login'");
+            } else {
+                $error = "El usuario $login está bloqueado durante 15 minutos.";
+                return $error;
+            }
         }
 
+        if (password_verify($password_login, $fila['password'])) {
+            pg_query($con, "UPDATE usuarios SET intentos_fallidos = 0, tiempo = NULL WHERE usuario = '$login'");
+            $_SESSION['datos_usuario'] = $datos_usuario;
+            header('Location: index.php');
+            exit();
+        } else {
+            $intentos_fallidos++;
+            pg_query($con, "UPDATE usuarios SET intentos_fallidos = $intentos_fallidos, tiempo = NOW() WHERE usuario = '$login'");
+            if($intentos_fallidos >= 3) {
+                pg_query($con, "UPDATE usuarios SET bloqueado = TRUE WHERE usuario = '$login'");
+            }
+            return "El usuario o la contraseña son incorrectos.";
+        }
     } else {
-        echo "No existen registros";
+        return "El usuario no existe.";
     }
 }
 
-if(isset($login, $password_login)){
-    comprobar($login, $password_login);
+if(isset($login, $password_login)) {
+    $error = comprobar($login, $password_login, $con);
 }
-
 ?>
 
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>Login</title>
+</head>
+
+<body>
+    <div class="backGradient" id='header'>
+        <div height='auto'>
+            <img class='logo_cab' src='imagenes/logo_blanco.png' alt="Logo">
+            <h3>GESTIÓN DE BAJAS Y ALTAS</h3>
+        </div>
+    </div>
+    <div class="acceso">
+        <p> ACCESO </p>
+    </div>
+    <form action="" method="POST">
+        <div>
+            <input class="login" type="text" name="login" id="login" placeholder="Usuario" required>
+            <input class="password" type="password" name="password_login" id="password_login" placeholder="Contraseña" required>
+        </div>
+        <?php if(isset($error)) { ?>
+            <div class="error"><?php echo $error; ?></div>
+        <?php } ?>
+        <button type="submit" class="boton"> Entrar </button>
+    </form>
+</body>
+
+</html>
